62740d5aced1e65dd6bd312fdcd4fa782e0df8c2
Kirill Rasuli <rasulikv@gmail.com>
2020-08-06 21:19:51 +0300

    Add image model

--- places/models.py
+++ places/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
+
 class Place(models.Model):
     title = models.CharField(max_length=255)
     description_short = models.TextField()
     description_long = models.TextField()
-    lat = models.DecimalField(max_digits=9, decimal_places=6)
-    lng = models.DecimalField(max_digits=9, decimal_places=6)
+    lat = models.DecimalField(max_digits=9, decimal_places=3)
+    lng = models.DecimalField(max_digits=9, decimal_places=3)
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='media/')
+    # title = models.CharField(max_length=255)
+
+    # def __str__(self):
+    #     return self.title
+
