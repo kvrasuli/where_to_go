aa18447c30882d5577dc7551fd744b7043768a1b
Kirill Rasuli <rasulikv@gmail.com>
2020-08-30 20:28:50 +0300

    Fix image model

--- places/models.py
+++ places/models.py
@@ -13,9 +13,13 @@ class Place(models.Model):
 
 
 class Image(models.Model):
+    title = models.ForeignKey(
+        Place,
+        on_delete=models.DO_NOTHING,
+        default=None,
+        null=True
+    )
     image = models.ImageField(upload_to='media/')
-    # title = models.CharField(max_length=255)
 
-    # def __str__(self):
-    #     return self.title
-
+    def __str__(self):
+        return f'{self.id}. {self.title}'
