05eab1b034a568fdce6cb1b9a3ae67a321417d92
Kirill Rasuli <rasulikv@gmail.com>
2020-09-08 11:34:40 +0300

    Change static settings in urls.py

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('tinymce/', include('tinymce.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
