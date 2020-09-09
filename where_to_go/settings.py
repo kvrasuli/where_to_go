347417cb63df08fc8120d1373a06a1aac9e608da
Kirill Rasuli <rasulikv@gmail.com>
2020-09-09 22:21:36 +0300

    Change settings

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -127,6 +127,6 @@ STATIC_ROOT = 'assets/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = ''
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
 
