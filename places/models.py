22481fc3057222b1d1fcdfa92c0b93e518955176
Kirill Rasuli <rasulikv@gmail.com>
2020-09-16 21:39:30 +0300

    Add verbose names to models' fields

--- places/models.py
+++ places/models.py
@@ -3,11 +3,11 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=255)
-    description_short = models.TextField()
-    description_long = HTMLField()
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    title = models.CharField(max_length=255, verbose_name='Название')
+    description_short = models.TextField(verbose_name='Краткое описание')
+    description_long = HTMLField(verbose_name='Подробное описание')
+    latitude = models.FloatField(verbose_name='Широта')
+    longitude = models.FloatField(verbose_name='Долгота')
 
     def __str__(self):
         return self.title
@@ -18,10 +18,14 @@ class Image(models.Model):
         Place,
         on_delete=models.CASCADE,
         default=None,
-        null=True
+        null=True,
+        verbose_name='Название'
     )
-    image = models.ImageField(upload_to='media/')
-    position = models.PositiveIntegerField(default=0, blank=False, null=False)
+    image = models.ImageField(upload_to='media/', verbose_name='Изображение')
+    position = models.PositiveIntegerField(
+        default=0, blank=False, null=False,
+        verbose_name='Позиция в списке'
+    )
 
     class Meta(object):
         ordering = ['position']
