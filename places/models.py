0dd566035d950beb8a109e89ce374c05ae504090
Kirill Rasuli <rasulikv@gmail.com>
2020-09-17 15:02:55 +0300

    Allow descriptions to be empty

--- places/models.py
+++ places/models.py
@@ -4,8 +4,8 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=255, verbose_name='Название')
-    description_short = models.TextField(verbose_name='Краткое описание')
-    description_long = HTMLField(verbose_name='Подробное описание')
+    description_short = models.TextField(verbose_name='Краткое описание', blank=True)
+    description_long = HTMLField(verbose_name='Подробное описание', blank=True)
     latitude = models.FloatField(verbose_name='Широта')
     longitude = models.FloatField(verbose_name='Долгота')
 
