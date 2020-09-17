37e34d8734ad15b4277f111dce8550eaf9b6c96a
Kirill Rasuli <rasulikv@gmail.com>
2020-09-17 20:34:41 +0300

    Fix a model (pep8)

--- places/models.py
+++ places/models.py
@@ -4,7 +4,9 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=255, verbose_name='Название')
-    description_short = models.TextField(verbose_name='Краткое описание', blank=True)
+    description_short = models.TextField(
+        verbose_name='Краткое описание', blank=True
+    )
     description_long = HTMLField(verbose_name='Подробное описание', blank=True)
     latitude = models.FloatField(verbose_name='Широта')
     longitude = models.FloatField(verbose_name='Долгота')
